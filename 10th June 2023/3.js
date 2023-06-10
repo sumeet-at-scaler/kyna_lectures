5973cb389179c6c8d47071be48c14f849bfb2656
sumeet-at-scaler <sumeet@scaler.com>
2023-06-10 15:48:45 +0530

    3rd example

---
+++ 10th June 2023/3.js
@@ -0,0 +1,22 @@
+let lo = 5;
+let hi = 15;
+
+/*
+let curr = lo;
+while(curr <= hi){
+    console.log(curr);
+    curr++;
+}
+*/
+
+/*
+for(let i = lo; i <= hi; i++){
+    console.log(i);
+}
+*/
+
+let curr = lo;
+do {
+    console.log(curr);
+    curr++;
+} while(curr <= hi);
